039dc1b537749d8c38fd601500846747bd4aed91
Oreonax <esok@mailoo.org>
2017-02-11 13:56:55 +0100

    prod bundle

---
+++ web/webpack.prod.config.js
@@ -0,0 +1,43 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+
+    './index.js'
+    // the entry point of our app
+  ],
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'dist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  context: resolve(__dirname, 'src'),
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [],
+}
